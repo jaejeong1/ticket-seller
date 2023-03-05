c8b11d1a83cb72de40c97f5c49a3db8cd3ec8f84
jaejeong1 <wowjd555@gmail.com>
2023-03-05 22:26:36 +0900

    회원가입 기능 및 테스트 구현

---
+++ src/test/java/com/jj/ticketseller/TicketSellerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.jj.ticketseller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TicketSellerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
